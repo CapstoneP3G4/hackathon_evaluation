3fe40ea1f173b73aa1b2cfffdda19bf3c2f93080
Vaibhav Agarwal <125553287+vaibhav-incedo@users.noreply.github.com>
2023-02-28 17:52:48 +0000

    Delete server-project-backend/fullstack-backend-master-2/fullstack-backend-master directory
    .

--- server-project-backend/fullstack-backend-master-2/fullstack-backend-master/src/main/java/com/fullstackbackend/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fullstackbackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fullstackbackend.model.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-}
